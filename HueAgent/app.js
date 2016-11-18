63718d5c6300dcfa0ffc04c5d78f39eff8a7c507
Thomas Conté <thomas@conte.com>
2016-11-18 13:46:51 +0100

    Connect to the Hue Bridge

--- HueAgent/app.js
+++ HueAgent/app.js
@@ -5,26 +5,66 @@ const Client = require('azure-iot-device').Client;
 const Message = require('azure-iot-device').Message;
 const request = require('request');
 
-// Get the Hue Bridge secret "username"
+/********************************************************************************/
+/* Initialization
+/********************************************************************************/
 
-// Register the device
+// Find the Hue Bridge
 
-request('http://localhost:8080/register/aaa-bbb-ccc-ddd-eee', function (error, response, body) {
-    console.log(body);
-    const deviceInfo = JSON.parse(body).deviceInfo;
-    const connectionString = "HostName=HueHub.azure-devices.net;DeviceId=" + deviceInfo.deviceId + ";SharedAccessKey=" + deviceInfo.authentication.symmetricKey.primaryKey;
-    startAgent(connectionString); 
-});
+request('https://www.meethue.com/api/nupnp', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log('Found the bridge!');
+        console.log(body);
+        let b = JSON.parse(body)[0];
+        let bridgeIp = b.internalipaddress;
+        let bridgeId = b.id;
 
-function startAgent(connectionString) {
+        // Now we need a whitelisted username for the Bridge
+        let userName;
+        // Option 1: the user has a username and enters it as a parameter
+        if (process.argv[2]) {
+            userName = process.argv[2];
+        }
+        // Option 2: the user doesn't have a username, let's create a new one
+        // TODO
+        
+        // Now register the bridge with the proxy service
+        registerAgent(bridgeIp, bridgeId, userName);
+    } else {
+        console.log(error);
+        return;
+    }
+})
+
+/********************************************************************************/
+/* Device registration
+/********************************************************************************/
+
+function registerAgent(ip, id, u) {
+    let uuid = id + '-' + u;
+    request('http://localhost:8080/register/' + uuid, function (error, response, body) {
+        const deviceInfo = JSON.parse(body).deviceInfo;
+        const connectionString = "HostName=HueHub.azure-devices.net;DeviceId=" + deviceInfo.deviceId + ";SharedAccessKey=" + deviceInfo.authentication.symmetricKey.primaryKey;
+        console.log('********************\nThis is your UUID: ' + uuid + '\n********************')
+        startAgent(ip, u, connectionString); 
+    });
+}
+
+/********************************************************************************/
+/* Agent
+/********************************************************************************/
+
+function startAgent(ip, user, connectionString) {
     // Connect directly to IoT Hub
     const client = Client.fromConnectionString(connectionString, Protocol);
     client.open(function() {
-        console.log('Client connected');
+        console.log('Client connected. Forwarding commands to ' + ip);
         client.on('message', function(msg) {
             console.log(msg.data);
-            client.complete(msg, function() {
-                console.log('Command acknowledged.');
+            executeCommand(ip, user, msg.data, function() {
+                client.complete(msg, function() {
+                    console.log('Command acknowledged.');
+                });
             });
         });
         client.on('error', function(err) {
@@ -35,4 +75,13 @@ function startAgent(connectionString) {
             client.open(connectCallback);
         });
     });
+}
+
+/********************************************************************************/
+/* Agent
+/********************************************************************************/
+
+function executeCommand(ip, user, command, cb) {
+    console.log('Send command ' + command.command + ' to ' + ip + ' with ' + user);
+    cb();
 }
